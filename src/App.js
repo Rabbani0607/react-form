57a80087e71c23190509b86c2ca98160c00776c6
Rabbani0607 <shaikrabbani0607@gmail.com>
2024-01-15 19:57:44 +0530

    add

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <label for="email">Email Id</label>
+      <input type="text" placeholder='Enter Email'/>
+      <br/>
+      <label for="password">Password</label>
+      <input type="text" placeholder='Enter Password'/>
+      <br/>
+      <input type="submit" placeholder='submit'/>
+    </div>
+  );
+}
+
+export default App;
